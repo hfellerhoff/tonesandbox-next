0408027d5f584e57fed2fa3c7fe7df0e33a0346e
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-05-02 11:49:00 -0500

    feat: swap SA for Umami

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,11 +9,16 @@ class MyDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=DM+Sans&display=swap'
             rel='stylesheet'
           />
-          <script
-            async
-            defer
-            src='https://stats.tonesandbox.com/latest.js'
-          ></script>
+          {process.env.VERCEL_ENV === 'production' ? (
+            <script
+              async
+              defer
+              data-website-id='e0135a7d-bd11-40a4-b153-336bccac133b'
+              src='https://umami.henryfellerhoff.com/umami.js'
+            ></script>
+          ) : (
+            <></>
+          )}
         </Head>
         <body>
           <Main />
